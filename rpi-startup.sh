65d7b48483514558aa7cedc5df13a652e6642c11
Will Ware <wware@alum.mit.edu>
2014-08-28 12:58:32 +0000

    Oops, fix redirection.

--- rpi-startup.sh
+++ rpi-startup.sh
@@ -2,7 +2,7 @@
 # This runs from /etc/rc.local on the Raspberry Pi, see
 # http://www.raspberry-projects.com/pi/pi-operating-systems/raspbian/scripts
 # Added the line:
-# /home/pi/CylindricalPrinter/rpi-startup.sh >& /home/pi/startup-log.txt &
+# /home/pi/CylindricalPrinter/rpi-startup.sh > /home/pi/startup-log.txt 2>&1 &
 
 cd /home/pi/CylindricalPrinter/software
 
