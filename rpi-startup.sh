2adc5d8270934245a90622ed67a544a62aaee360
Will Ware <wware@alum.mit.edu>
2014-09-04 20:46:51 +0000

    The startup script should run as "pi", not "root".

--- rpi-startup.sh
+++ rpi-startup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # This runs from /etc/rc.local on the Raspberry Pi, see
 # http://www.raspberry-projects.com/pi/pi-operating-systems/raspbian/scripts
-# Added the line:
-# /home/pi/CylindricalPrinter/rpi-startup.sh > /home/pi/startup-log.txt 2>&1 &
+#
+# Added the lines:
+#    su pi << EOF
+#    nohup /home/pi/CylindricalPrinter/rpi-startup.sh > /home/pi/startup-log.txt 2>&1 &
+#    EOF
 
 cd /home/pi/CylindricalPrinter/software
 
