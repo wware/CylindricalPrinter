8d4ffc60ab914a3a7719ae61ea43da479ccb2732
Will Ware <wware@alum.mit.edu>
2014-09-12 16:53:00 -0400

    calibration results

--- software/config.py
+++ software/config.py
@@ -28,19 +28,24 @@ STEPS_PER_SLICE = int(0.01 * STEPS_PER_INCH)
 # fresh uncured resin. No need to travel far.
 #HYSTERESIS_STEPS = 2 * STEPS_PER_SLICE
 HYSTERESIS_STEPS = 0
-POST_MOVE_SETTLING_TIME = 5   # seconds
+POST_MOVE_SETTLING_TIME = 10   # seconds
 
 # This is the XY scale factor, increasing this number makes the projected
 # shape smaller linearly. This number of STL units in the X and Y direction
-# maps to one projector screen width.
-XYSCALE = 230
+# maps to one projector screen width. The best way to calibrate is to print
+# with test objects with known dimensions, and measure the results with a
+# micrometer. NOTE - this number will vary with the height of liquid in the
+# bucket, as will the focus setting of the projector!
+XYSCALE = 244
 
 if PRACTICE_MODE:
     EXPOSURETIME = 1000
 else:
     EXPOSURETIME = 60000    # milliseconds
 
-# This is in millimeters, a hundredth of an inch.
+# This is in millimeters, a hundredth of an inch. It's millimeters because all
+# the STL dimensions in printer.py are in millimeters, the de-facto standard
+# unit for STL files as far as I have seen.
 SLICE_THICKNESS = 0.254
 
 
