69df6da084373227eb55c6da914f6b1874abe679
Will Ware <wware@alum.mit.edu>
2015-02-03 18:08:46 -0500

    more puttering

--- S80cylpri.sh
+++ S80cylpri.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
+mount /mnt/usbstick || exit 1
 cd /home/root
 echo "Starting cylindrical printer at $(date)" > T.txt
 cd CylindricalPrinter/software
-exec python2.7 rpi.py
+exec python2.7 rpi.py &
