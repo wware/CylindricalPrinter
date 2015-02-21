22c80a51bc478ba91349ff5f4f17ffd70e7935c5
Will Ware <wware@alum.mit.edu>
2015-02-21 17:18:01 -0500

    A bit more work on the startup script.

--- S80cylpri.sh
+++ S80cylpri.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
+#!/bin/bash -e
 
-mount /mnt/usbstick || exit 1
+LOGFILE=/home/root/cp-log.txt
+rm -f $LOGFILE
+
+mount /mnt/usbstick 2>/dev/null || echo "Can't mount USB stick. Already mounted?" >> $LOGFILE
 cd /home/root
-echo "Starting cylindrical printer at $(date)" > T.txt
+echo "Starting cylindrical printer at $(date)" >> $LOGFILE
 cd CylindricalPrinter/software
-exec python2.7 rpi.py &
+exec python2.7 rpi.py > $LOGFILE 2>&1 &
